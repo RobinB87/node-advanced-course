eaad6a1ab6511ee7ff537753d1a1e5e5ee578876
RobinB87 <robinbette@hotmail.com>
2023-11-09 15:00:54 +0000

    explanation of the thread pool

--- main/threads.js
+++ main/threads.js
@@ -8,6 +8,10 @@ crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
 
 // both these pbkdf2 methods are executed more or less at the exact same time.
 // this second one does NOT wait for the first one to be finished
+// the computation in the pbkdf2 is delegated to the c++ libuv library, which makes use of a thread pool:
+//  - so node + libuv might compute outside of the single thread
+//  - the thread pool has 4 threads that can be used to run computational intensive tasks
+//  - so next to the event loop, there are 4 other threads that can be used to offload some heavy work
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log("2:", Date.now() - start);
 });
