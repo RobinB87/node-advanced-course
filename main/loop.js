6805151631d9e9c6d7529788017583037480a3a2
RobinB87 <robinbette@hotmail.com>
2023-11-09 15:12:14 +0100

    add some checks

--- main/loop.js
+++ main/loop.js
@@ -4,7 +4,11 @@
 myFile.runContents();
 
 function shouldContinue() {
-  // 
+  // check one: any pending setTimeout, setInterval, setImmediated?
+  // if one of these functions is still pending, our program does not exit
+
+  // check two: any pending OS tasks? (like server listening to port)
+  // check three: any pending long running operations (like fs module)
 }
 
 // every time the event loop runs, the entire body of this while loop executes in one 'tick'
