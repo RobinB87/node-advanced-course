c69e8d4b923000c471546aeb5f3729a82161d403
RobinB87 <robinbette@hotmail.com>
2023-11-09 15:57:41 +0000

    question about the order of the logs logs

--- main/multitask.js
+++ main/multitask.js
@@ -1,7 +1,19 @@
 const crypto = require("crypto");
 const https = require("https");
+const fs = require("fs");
 
 const start = Date.now();
+
+function doRequest() {
+  https
+    .request("https://www.google.com", (res) => {
+      res.on("data", () => {});
+      res.on("end", () => {
+        console.log("HTTP:", Date.now() - start);
+      });
+    })
+    .end();
+}
 
 function doHash() {
   crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
@@ -9,13 +21,13 @@ function doHash() {
   });
 }
 
-function doRequest() {
-  https
-    .request("https://www.google.com", (res) => {
-      res.on("data", () => {});
-      res.on("end", () => {
-        console.log(Date.now() - start);
-      });
-    })
-    .end();
-}
+doRequest();
+
+fs.readFile("multitask.js", "utf8", () => {
+  console.log("FS:", Date.now() - start);
+});
+
+doHash();
+doHash();
+doHash();
+doHash();
