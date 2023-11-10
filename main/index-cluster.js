f37ac1f845b167d4739345d1ec5e78e785e2affb
RobinB87 <robinbette@hotmail.com>
2023-11-10 08:24:42 +0000

    set up extra clusters and see what happens

--- main/index-cluster.js
+++ main/index-cluster.js
@@ -3,7 +3,10 @@ const cluster = require("cluster");
 // is the file being executed in master mode?
 if (cluster.isMaster) {
   // cause index.js to be executed *again*, but in child mode
-  cluster.fork();
+  cluster.fork(); // if you comment this one out as well, the server immediately stops
+  // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
 } else {
   // I am a child, I am going to act like a server and nothing else
   const express = require("express");
@@ -17,6 +20,10 @@ if (cluster.isMaster) {
   app.get("/", (req, res) => {
     doWork(5000);
     res.send("hi there");
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("this was fast!");
   });
 
   app.listen(3000);
