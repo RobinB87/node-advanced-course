e12f28b078ae525ecadad98b7a6fff49fa099c2c
RobinB87 <robinbette@hotmail.com>
2023-11-15 07:23:19 +0000

    add input for image

--- AdvancedNodeStarter/client/src/components/blogs/BlogFormReview.js
+++ AdvancedNodeStarter/client/src/components/blogs/BlogFormReview.js
@@ -1,10 +1,10 @@
 // BlogFormReview shows users their form inputs for review
-import _ from 'lodash';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import formFields from './formFields';
-import { withRouter } from 'react-router-dom';
-import * as actions from '../../actions';
+import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import formFields from "./formFields";
+import { withRouter } from "react-router-dom";
+import * as actions from "../../actions";
 
 class BlogFormReview extends Component {
   renderFields() {
@@ -25,10 +25,7 @@ class BlogFormReview extends Component {
 
     return (
       <div>
-        <button
-          className="yellow darken-3 white-text btn-flat"
-          onClick={onCancel}
-        >
+        <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
           Back
         </button>
         <button className="green btn-flat right white-text">
@@ -52,6 +49,9 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add an image</h5>
+        <input type="file" accept="image/*" />
 
         {this.renderButtons()}
       </form>
