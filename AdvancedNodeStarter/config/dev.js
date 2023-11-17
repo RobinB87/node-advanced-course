983ca57243340532a87b3cd0f9f1c9d3db64b6ce
RobinB87 <robinbette@hotmail.com>
2023-11-17 07:50:35 +0000

    delete dev.js

--- AdvancedNodeStarter/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-  mongoURI: "mongodb://db:27017/blog_dev?readPreference=primary&ssl=false&directConnection=true",
-  cookieKey: "123123123",
-};
