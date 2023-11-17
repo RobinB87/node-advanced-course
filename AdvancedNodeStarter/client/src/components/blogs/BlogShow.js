fac4ea4fcec21edfc1064f3e2c6f151513cba6b8
RobinB87 <robinbette@hotmail.com>
2023-11-17 12:26:31 +0000

    show image

--- AdvancedNodeStarter/client/src/components/blogs/BlogShow.js
+++ AdvancedNodeStarter/client/src/components/blogs/BlogShow.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchBlog } from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchBlog } from "../../actions";
 
 class BlogShow extends Component {
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+      const url = "https://my-blog-bucket-temp.s3.eu-central-1.amazonaws.com";
+      return <img src={`${url}/${this.props.blog.imageUrl}`} />;
+    }
+  }
+
   render() {
     if (!this.props.blog) {
-      return '';
+      return "";
     }
 
     const { title, content } = this.props.blog;
@@ -18,6 +25,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
